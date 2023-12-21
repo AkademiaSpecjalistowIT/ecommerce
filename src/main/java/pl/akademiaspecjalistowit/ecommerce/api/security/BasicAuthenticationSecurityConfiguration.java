c2d3ee4308623acc5d8dd8f1f3d0cb8b153ce647
przemyslawgesieniecasit <przemyslaw.gesieniec@gmail.com>
2023-12-21 05:05:29 +0100

    basic authentication with rest api, csrf disabled

--- src/main/java/pl/akademiaspecjalistowit/ecommerce/api/security/BasicAuthenticationSecurityConfiguration.java
+++ src/main/java/pl/akademiaspecjalistowit/ecommerce/api/security/BasicAuthenticationSecurityConfiguration.java
@@ -50,42 +50,4 @@ public class BasicAuthenticationSecurityConfiguration {
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
-
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//            .authorizeHttpRequests((authorize) -> authorize
-//                .requestMatchers("/login").permitAll()
-//                .requestMatchers("/orders").hasRole("CLIENT_ROLE")
-//                .anyRequest().authenticated()
-//            );
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public AuthenticationManager authenticationManager(
-//        UserDetailsService userDetailsService,
-//        PasswordEncoder passwordEncoder) {
-//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-//        authenticationProvider.setUserDetailsService(userDetailsService);
-//        authenticationProvider.setPasswordEncoder(passwordEncoder);
-//
-//        return new ProviderManager(authenticationProvider);
-//    }
-//
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails userDetails = User.withDefaultPasswordEncoder()
-//            .username("user")
-//            .password(passwordEncoder().encode("password"))
-//            .roles("CLIENT_ROLE")
-//            .build();
-//
-//        return new InMemoryUserDetailsManager(userDetails);
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//    }
 }
