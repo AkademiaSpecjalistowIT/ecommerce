2830c026456a1dcbb1a3dd0ae43e9aed7da7b400
przemyslawgesieniecasit <przemyslaw.gesieniec@gmail.com>
2023-12-21 05:05:02 +0100

    basic authentication with rest api, csrf disabled

--- src/main/java/pl/akademiaspecjalistowit/ecommerce/api/ClientController.java
+++ src/main/java/pl/akademiaspecjalistowit/ecommerce/api/ClientController.java
@@ -2,17 +2,23 @@ package pl.akademiaspecjalistowit.ecommerce.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.akademiaspecjalistowit.api.ClientApi;
+import pl.akademiaspecjalistowit.api.ClientsApi;
 import pl.akademiaspecjalistowit.model.AddFundsRequest;
 
 @Slf4j
 @RestController
-public class ClientController implements ClientApi {
+public class ClientController implements ClientsApi {
 
     @Override
     public ResponseEntity<Void> addFunds(AddFundsRequest addFundsRequest) {
         log.info("User triggered founds upgrade: {} ", addFundsRequest.toString());
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/orders")
+    public String orders(){
+        return "jest okej";
     }
 }
